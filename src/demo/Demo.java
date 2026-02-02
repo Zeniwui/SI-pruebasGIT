7fd7f6a9917cbd09c2d3262b81cfcbfaec7369c0
Xen <aron.laika@gmail.com>
2026-02-02 13:56:23 +0100

    Estado inicial ejercicio 4

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,7 +2,7 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("cambio rama master para rebase");		
+		System.out.println("linea 1");		
 		System.out.println("linea 2");
 		System.out.println("linea 3");		
 	}
